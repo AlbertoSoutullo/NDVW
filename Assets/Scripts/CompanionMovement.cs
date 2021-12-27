b05ba8d75e7d89033ff96f336f7bed997130afaa
adriantormos <adriantormos98@gmail.com>
2021-12-27 18:37:43 +0100

    Add some FSM states
    Adds implementations for the Idle, FollowPlayer, ChooseTarget and Recharge states (still work in progress though). A NullPointerException can happen because the Relocate state is still missing.

--- Assets/Scripts/CompanionMovement.cs
+++ Assets/Scripts/CompanionMovement.cs
@@ -1,27 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using System.Linq;
+using System.Collections.Generic;
 
 public class CompanionMovement : MonoBehaviour
 {
 	FiniteStateMachine<CompanionMovement> stateMachine;
 
-	public float playerMaxDistance = 20;
-    public float Speed;
-    
-    public Transform player;
-    
-    // Start is called before the first frame update
-    void Start()
+	public float playerMaxDistance = 5;
+	public float weaponRangeDistance = 8;
+	public int arrows = 10;
+	public float visionRangeDistance = 25;
+	public bool weaponIsCharged = true;
+	public GameObject currentTarget = null;
+	public float Speed;
+
+	public Transform player;
+
+	public FiniteStateMachine<CompanionMovement> GetFSM()
+	{
+		return stateMachine;
+	}
+
+	public IEnumerable<GameObject> SeeEnemies()
+	{
+		return GameObject.FindGameObjectsWithTag("Enemy").Where(enemy => Vector3.Distance(transform.position, enemy.transform.position) < visionRangeDistance);
+	}
+
+	// An enemy can be attacked if the companion can locate itself than weaponRangeDistance
+	// while maintaining itself closer than playerMaxDistance to the player
+	public IEnumerable<GameObject> EnemiesThatCanBeAttacked()
+	{
+		return GameObject.FindGameObjectsWithTag("Enemy")
+			             .Where(enemy => Vector3.Distance(transform.position, enemy.transform.position) < visionRangeDistance
+						                 && Vector3.Distance(player.position, enemy.transform.position) < (weaponRangeDistance + playerMaxDistance));
+	}
+
+	// Enemies that are closer than weaponRangeDistance to the companion
+	public IEnumerable<GameObject> EnemiesInWeaponRange()
+	{
+		return GameObject.FindGameObjectsWithTag("Enemy")
+						 .Where(enemy => Vector3.Distance(transform.position, enemy.transform.position) < weaponRangeDistance);
+	}
+
+	// Start is called before the first frame update
+	void Start()
 	{
 		stateMachine = new FiniteStateMachine<CompanionMovement>(this);
-		stateMachine.CurrentState = FollowPlayerState.Instance;
+		stateMachine.CurrentState = IdleState.Instance;
 	}
 
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 		stateMachine.Update();
-    }
+	}
 }
