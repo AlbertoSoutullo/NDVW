4740cbaa01c76a430a368db5f817f3552b47c4e4
AlbertoSoutullo <alberto.soutullo@hotmail.com>
2022-01-05 15:39:46 +0100

    Health bar is not facing camera. Created prefabs from hunter, red riding and enemy.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,27 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    Quaternion startRotation;
-    
+
     public Image healthBarImage;
     public Player player;
     public Color fullColor;
     public Color lowColor;
 
+    public Camera Camera;
+
+    private void Update()
+    {
+        transform.parent.LookAt(transform.position + Camera.transform.rotation * Vector3.back, 
+            Camera.transform.rotation * Vector3.down);
+    }
+
     public void UpdateHealthBar()
     {
-        Debug.Log(fullColor.ToString()); Debug.Log(lowColor.ToString());
-        Debug.Log(healthBarImage.ToString()); Debug.Log(player.ToString());
+        // Debug.Log(fullColor.ToString()); Debug.Log(lowColor.ToString());
+        // Debug.Log(healthBarImage.ToString()); Debug.Log(player.ToString());
         healthBarImage.fillAmount = Mathf.Clamp(player.health / player.maxHealth, 0, 1f);
         healthBarImage.color = Color.Lerp(lowColor, fullColor, healthBarImage.fillAmount);
+        
     }
 
 }
